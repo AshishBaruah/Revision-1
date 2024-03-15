2a9a8496a03c79724b8a18a507d8c2632ca4f110
Ashish Baruah <ashishbaruah90@gmail.com>
2024-03-15 23:18:31 +0530

    Added Day 2 files created

---
+++ REVISION SESSION 5.py
@@ -0,0 +1,44 @@
+"""
+    This contains all the files created while revising STRINGS in python.
+"""
+"""
+    TOPICS COVERED ARE :
+    - strings basics
+    - String indexing
+    - String slicing
+    - edit and delete string
+    - operations on string
+    - common string functions
+
+"""
+
+"""
+    TOPIC : STRINGS BASICS
+    - In python, strings are a sequence of unicode characters
+    - Unicode is based on 16 bit characters.
+"""
+
+s = "hello"
+s = 'hello'
+#   multline strings
+s = '''hello'''
+s = '''hello'''
+# by type converting
+s1 = str('hello')
+
+# print("it's raining outside")
+
+# Accessing Substrings from a string
+
+# positive indexing
+s2 = "hello world"
+# print(s2[0])
+
+# negative indexing
+# python assigns last index as -1 and so on...
+s3 = "hello world"
+
+print(s3[-1])
+print(s3[-3])
+
+# 
